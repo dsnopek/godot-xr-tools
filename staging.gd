71f7e18e9263689317598ed48a31f62cd9982577
David Snopek <dsnopek@gmail.com>
2022-12-08 17:38:41 -0600

    Got running initially in WebXR

--- staging.gd
+++ staging.gd
@@ -24,10 +24,56 @@ extends XRToolsStaging
 
 var scene_is_loaded : bool = false
 
+var webxr_interface: WebXRInterface
+var webxr_vr_is_supported := false
+
 # Called when this handle is added to the scene
 func _ready() -> void:
 	# In Godot 4 we must now manually call our super class ready function
 	super._ready()
+
+	webxr_interface = XRServer.find_interface('WebXR')
+	if webxr_interface:
+		webxr_interface.session_supported.connect(self._on_webxr_session_supported)
+		webxr_interface.session_started.connect(self._on_webxr_session_started)
+		webxr_interface.session_ended.connect(self._on_webxr_session_ended)
+		webxr_interface.session_failed.connect(self._on_webxr_session_failed)
+
+		webxr_interface.is_session_supported("immersive-vr")
+
+
+func _on_webxr_session_supported(session_mode: String, supported: bool) -> void:
+	if session_mode == "immersive-vr":
+		webxr_vr_is_supported = supported
+		if webxr_vr_is_supported:
+			%WebXRLayer.visible = true
+		else:
+			OS.alert("Your web browser doesn't support VR. Sorry!")
+
+
+func _on_webxr_session_started() -> void:
+	%WebXRLayer.visible = false
+	get_viewport().use_xr = true
+
+
+func _on_webxr_session_ended() -> void:
+	%WebXRLayer.visible = true
+	get_viewport().use_xr = false
+
+
+func _on_webxr_session_failed(message: String) -> void:
+	OS.alert("Unable to enter VR: " + message)
+	%WebXRLayer.visible = true
+
+
+func _on_webxr_enter_vr_button_pressed():
+	webxr_interface.session_mode = 'immersive-vr'
+	webxr_interface.requested_reference_space_types = 'bounded-floor, local-floor, local'
+	webxr_interface.required_features = 'local-floor'
+	webxr_interface.optional_features = 'bounded-floor'
+
+	if not webxr_interface.initialize():
+		OS.alert("Failed to initialize")
 
 
 func _on_Staging_scene_loaded(scene):
@@ -59,8 +105,8 @@ func _on_XROrigin3D_session_synchronized():
 	# We get the synchronized state at startup and whenever the player
 	# removes their headset (or goes into the menu system).
 	#
-	# If the user doesn't put their headset on again before we load a 
-	# new scene, we'll want to show the prompt so we don't load the 
+	# If the user doesn't put their headset on again before we load a
+	# new scene, we'll want to show the prompt so we don't load the
 	# next scene in while the player is still adjusting their position
 	prompt_for_continue = true
 
