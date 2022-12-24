f3a539f087ca94db21397782c560ce685e487473
David Snopek <dsnopek@gmail.com>
2022-12-24 10:31:09 -0600

    Get working in all the demo scenes

--- scenes/main_menu/main_menu_level.gd
+++ scenes/main_menu/main_menu_level.gd
@@ -1,5 +1,5 @@
 @tool
-extends XRToolsSceneBase
+extends "../scene_base.gd"
 
 func _update_demo_positions() -> void:
 	var count = $Demos.get_child_count()
@@ -14,22 +14,8 @@ func _update_demo_positions() -> void:
 
 
 func _ready():
+	super._ready()
 	_update_demo_positions()
-
-	var webxr_interface = XRServer.find_interface("WebXR")
-	if webxr_interface:
-		XRToolsUserSettings.webxr_primary_changed.connect(self._on_webxr_primary_changed)
-		_on_webxr_primary_changed(XRToolsUserSettings.get_real_webxr_primary())
-
-
-func _on_webxr_primary_changed(webxr_primary: int) -> void:
-	# Default to thumbstick.
-	if webxr_primary == 0:
-		webxr_primary = XRToolsUserSettings.WebXRPrimary.THUMBSTICK
-	var action_name = XRToolsUserSettings.WebXRPrimaryName[webxr_primary]
-	%LeftHand.get_node("MovementDirect").input_action = action_name
-	%RightHand.get_node("MovementDirect").input_action = action_name
-	%RightHand.get_node("MovementTurn").input_action = action_name
 
 
 func _on_Demos_child_entered_tree(_node):
